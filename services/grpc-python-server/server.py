08bcf5877d14b3234b171f56f53d8f8c32a96721
nagi <nagi>
2024-11-16 18:41:20 +0000

    got a simple server working

--- services/grpc-python-server/server.py
+++ services/grpc-python-server/server.py
@@ -1,10 +1,26 @@
 import asyncio
 import grpc
+from faker import Faker
+# imports from auto-generated files
+from ask_random_names_pb2_grpc import RandomNamesServicer, add_RandomNamesServicer_to_server
+from ask_random_names_pb2 import RandomNamesResponse
+
+
+fake = Faker()
+Faker.seed(0)
+
+
+class RandNameService(RandomNamesServicer):
+    def Names(self, request, context):
+        fake_names = [fake.name() for _ in range(request.max_results)]
+        return RandomNamesResponse(names=fake_names)
 
 
 async def serve():
     server = grpc.aio.server()
+    add_RandomNamesServicer_to_server(RandNameService(), server)
     server.add_insecure_port("0.0.0.0:50051")
+    print("HOLA")
     await server.start()
     await server.wait_for_termination()
 
