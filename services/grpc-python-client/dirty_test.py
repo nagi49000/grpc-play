174cf3be477cc558632818944f4c31cd70db24ad
nagi <nagi>
2024-11-17 12:38:27 +0000

    refactored Dockerfile for test and made server and port configurable in client

--- services/grpc-python-client/dirty_test.py
+++ services/grpc-python-client/dirty_test.py
@@ -1,10 +1,11 @@
+from os import getenv
 from time import sleep
 import grpc
 from ask_random_names_pb2 import RandomNamesRequest
 from ask_random_names_pb2_grpc import RandomNamesStub
 
 sleep(1)
-channel = grpc.insecure_channel("grpc-python-server:50051")
+channel = grpc.insecure_channel(f"{getenv('GRPC_SERVER_FQDN', 'localhost')}:{getenv('GRPC_SERVER_PORT', '50051')}")
 client = RandomNamesStub(channel)
 request = RandomNamesRequest(max_results=5)
 print(client.Names(request))
