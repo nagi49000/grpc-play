184628f0ddd7cfe7bc348b2e51692d0e90652591
nagi <nagi>
2024-11-30 21:05:02 +0000

    updated nginx set up to use TLS

--- services/grpc-python-client/dirty_test.py
+++ services/grpc-python-client/dirty_test.py
@@ -1,4 +1,5 @@
 from os import getenv
+from pathlib import Path
 from time import sleep
 import grpc
 from ask_random_names_pb2 import RandomNamesRequest, RandomCitiesRequest
@@ -17,7 +18,16 @@ print(client.Cities(request))
 nginx_server = getenv("NGINX_GRPC_GATEWAY_FQDN", None)
 nginx_port = getenv("NGINX_GRPC_GATEWAY_PORT", None)
 if nginx_server and nginx_port:
-    channel = grpc.insecure_channel(f"{nginx_server}:{nginx_port}")
+    try:
+        with open(Path("/") / "cacerts" / "cert.pem", "rb") as f:
+            channel_creds = grpc.ssl_channel_credentials(f.read())
+        print("CA certs found, using secure_channel")
+        channel = grpc.secure_channel(f"{nginx_server}:{nginx_port}", channel_creds)
+
+    except FileNotFoundError:
+        print("CA certs not found, using insecure_channel")
+        channel = grpc.insecure_channel(f"{nginx_server}:{nginx_port}")
+
     client = RandomNamesStub(channel)
-    request = RandomNamesRequest(max_results=2)
+    request = RandomNamesRequest(max_results=3)
     print(client.Names(request))
--- services/nginx/README.md
+++ services/nginx/README.md
@@ -2,9 +2,7 @@
 
 Gateway for grpc, with TLS.
 
-Key and self signed cert generated with 
+Key and self signed cert generated with
 ```
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"
-
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=grpc-nginx"
 ```
-
